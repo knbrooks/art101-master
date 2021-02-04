29a4106951b4c8f4af970ff70c373aecfd06f4d9
knbrooks <knbrooks@ucsc.edu>
2021-02-03 17:32:07 -0800

    week5lab7

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,24 +1,23 @@
+  /**
+      *lab 6 Arrays and Objects
+      *Author:Katelyn Brooks
+      *Created:1 30 2021
+      *License: Public Domain
+    **/
 
-/**
-  *lab 6 Arrays and Objects
-  *Author:Katelyn Brooks
-  *Created:1 30 2021
-  *License: Public Domain
-**/
+    //Define Variables
+    var myTransport = [" bus", " car", " bike", " walking"];
 
-//Define Variables
-var myTransport = [" bus", " car", " bike", " walking"];
-
-var myMainRide = {
-    make: "Honda",
-    model: "CR-V",
-    year: 2015,
-    age: function() {
-      return 2021 - age;
+    var myMainRide = {
+        make: "Honda",
+        model: "CR-V",
+        year: 2015,
+        age: function() {
+          return 2021 - age;
+        }
     }
-}
 
-//output
-document.writeln("My Transportation Methods: " + myTransport + "<br>");
-document.writeln("My Main Ride: <pre>",
-    JSON.stringify (myMainRide, null, '\t'), "</pre>");
+    //output
+    document.writeln("My Transportation Methods: " + myTransport + "<br>");
+    document.writeln("My Main Ride: <pre>",
+        JSON.stringify (myMainRide, null, '\t'), "</pre>");
